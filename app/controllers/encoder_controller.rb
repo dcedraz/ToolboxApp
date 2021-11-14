25e153000faf65d7ce7d6e312e88ce87b6710f88
dcedraz <dcedraz@salsify.com>
2021-11-14 22:53:09 +0000

    Fix the verify method and upload page

--- app/controllers/encoder_controller.rb
+++ app/controllers/encoder_controller.rb
@@ -3,7 +3,7 @@ class EncoderController < ApplicationController
   end
 
   def uploads
-    upload(uploads_params)
+    @result = verify(uploads_params)
   end
 
   def upload(userfile)
@@ -12,6 +12,20 @@ class EncoderController < ApplicationController
     file.write(uploaded_file.read)
     end
   end
+
+  def verify(userfile)
+    # Note: use form validation to ensure that
+    # params[:uploaded_file] is not null
+    file_data = userfile
+    if file_data.respond_to?(:read)
+      file_contents = file_data.read
+    elsif file_data.respond_to?(:path)
+      file_contents = File.read(file_data.path)
+    else
+      logger.error "Bad file_data: #{file_data.class.name}: #{file_data.inspect}"
+    end
+  end
+
   private
 
   # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
   get 'encoder/uploads'
+  post 'encoder/verify'
   post 'encoder/uploads'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
